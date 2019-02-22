d575ec92148f547af92bdf0de06959e255e50368
Zhuo Lily Wang <zw2519@columbia.edu>
2019-02-22 03:10:57 -0500

    clean data and lag price

--- data_cleaning.m
+++ data_cleaning.m
@@ -14,14 +14,10 @@ JSE.Dates = x2mdate(JSE.Dates,0);
 JSE.Dates = datetime(JSE.Dates,'ConvertFrom','datenum');
 SP.Dates = x2mdate(SP.Dates,0);
 SP.Dates = datetime(SP.Dates,'ConvertFrom','datenum');
-JSE_SP = outerjoin(JSE,SP,'MergeKeys',true);  
+JSE_SP = outerjoin(SP,JSE,'MergeKeys',true);  
 % merge tables
 MergedTable = outerjoin(JALSHRatios,JSE_SP,'MergeKeys',true);  
-% lag 'JSE_Close_Price'&'JSE_Total_Return' & 'JSE_Period_Return'
-JSE_lagged1 = circshift(MergedTable.JSE_Close_Price,1);
-JSE_lagged2 = circshift(MergedTable.JSE_Total_Return,1);
-JSE_lagged3 = circshift(MergedTable.JSE_Period_Return,1);  
-%merge tables
-LagTable = table(JSE_lagged1,JSE_lagged2,JSE_lagged3);
-% final dataset
-dataReady =[MergedTable, LagTable];
+% lag 'JSE_Close_Price'&'JSE_Total_Return' & 'JSE_Period_Return
+MergedTable(2:end,283:285)=MergedTable(1:end-1,283:285);
+MergedTable{1,283:285}= NaN([1,3]);
+
